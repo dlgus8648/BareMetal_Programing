e5a591fd9a4e5cb79c70220f647cf90b14e87ee3
KIM_RI_HYEON <dlgus8648@naver.com>
2024-12-04 22:02:09 +0900

    Update i2c.c

--- LAB1_I2C(ADXL345)/src/i2c.c
+++ LAB1_I2C(ADXL345)/src/i2c.c
@@ -5,27 +5,26 @@
 #define 	GPIOBEN			(1U<<1)
 #define 	I2C1EN			(1U<<21)
 
-#define 	I2C_100KHZ						80		
-#define 	SD_MODE_MAX_RISE_TIME			17
+#define 	I2C_100KHZ		80		
+#define 	SD_MODE_MAX_RISE_TIME	17
 #define		CR1_PE			(1U<<0)
 
 #define		SR2_BUSY		(1U<<1)
-#define     CR1_START		(1U<<8)
+#define    	 CR1_START		(1U<<8)
 #define		SR1_SB			(1U<<0)
 #define		SR1_ADDR		(1U<<1)
 #define		SR1_TXE			(1U<<7)
 #define		CR1_ACK			(1U<<10)
-#define     CR1_STOP		(1U<<9)
+#define     	CR1_STOP		(1U<<9)
 #define		SR1_RXNE		(1U<<6)
-#define	    SR1_BTF			(1U<<2)
+#define	   	SR1_BTF			(1U<<2)
 
 /*
  * PB8 ---- SCL
  * PB9 ----- SDA
  * */
 //------------------------------------------------------------//
-void I2C1_init(void)
-{
+void I2C1_init(void){
 //------------------------------------------------------------//
 	 RCC->AHB1ENR |=GPIOBEN;
 //------------------------------------------------------------//
@@ -67,7 +66,7 @@ void I2C1_init(void)
 }
 
 void I2C1_byteRead(char saddr, char maddr, char* data) {
-
+	
       volatile int tmp;
 //------------------------------------------------------------//
 	  while (I2C1->SR2 & (SR2_BUSY)){}
@@ -108,7 +107,6 @@ void I2C1_byteRead(char saddr, char maddr, char* data) {
 void I2C1_burstRead(char saddr, char maddr, int n, char* data) {
 
 	volatile int tmp;
-
 //------------------------------------------------------------//
 	  while (I2C1->SR2 & (SR2_BUSY)){}
 //------------------------------------------------------------//
@@ -145,7 +143,6 @@ void I2C1_burstRead(char saddr, char maddr, int n, char* data) {
     	
     	if(n == 1U)
     	{
- 
     	    I2C1->CR1 &= ~CR1_ACK;
     	    I2C1->CR1 |= CR1_STOP;
 			
@@ -168,10 +165,9 @@ void I2C1_burstRead(char saddr, char maddr, int n, char* data) {
 
 void I2C1_burstWrite(char saddr, char maddr, int n, char* data) {
 
-	volatile int tmp;
-
+    volatile int tmp;
 //------------------------------------------------------------//
-	 while (I2C1->SR2 & (SR2_BUSY)){}
+    while (I2C1->SR2 & (SR2_BUSY)){}
 //------------------------------------------------------------//
     I2C1->CR1 |= CR1_START;
 //------------------------------------------------------------//
