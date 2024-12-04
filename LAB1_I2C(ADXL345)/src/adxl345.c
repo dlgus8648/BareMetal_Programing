1913f69103857c07ce886a3aceb12efd487b9ad8
KIM_RI_HYEON <dlgus8648@naver.com>
2024-12-04 22:45:58 +0900

    Update adxl345.c

--- LAB1_I2C(ADXL345)/src/adxl345.c
+++ LAB1_I2C(ADXL345)/src/adxl345.c
@@ -3,34 +3,30 @@
 
 
 
-char data;
+ char data;
  uint8_t data_rec[6];
 
-void adxl_read_address (uint8_t reg)
-{
+void adxl_read_address (uint8_t reg){
+	
 	 I2C1_byteRead( DEVICE_ADDR, reg, &data);
-
 }
 
-void adxl_write (uint8_t reg, char value)
-{
+void adxl_write (uint8_t reg, char value){
+	
 	char data[1];
 	data[0] = value;
-
 	I2C1_burstWrite( DEVICE_ADDR, reg,1, data) ;
 }
 
-void adxl_read_values (uint8_t reg)
-{
+void adxl_read_values (uint8_t reg){
+	
 	I2C1_burstRead(DEVICE_ADDR, reg, 6,(char *)data_rec);
-
 }
 
 
-void adxl_init (void)
-{
+void adxl_init (void){
+	
 	I2C1_init();
-
 	adxl_read_address(DEVID_R);
 	adxl_write (DATA_FORMAT_R, FOUR_G);
 	adxl_write (POWER_CTL_R, RESET);
